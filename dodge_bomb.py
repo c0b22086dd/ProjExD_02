369e079c7463c9750214f52fd7e4de65383093bd
C0B22086 <c0b22086dd@edu.teu.ac.jp>
2023-06-20 14:11:20 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,8 +12,17 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20, 20))  #練習1
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_img.set_colorkey((0, 0, 0))
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    bd_rect = bg_img.get_rect()
+    bd_rect.center = x, y  #爆弾rectの中心座標を乱数で指定
+    
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +30,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img, bd_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
